a4825ab2b2f1f56ca2a63737f94c71880432b1a9
Nnadi Victory <chikamagozi25@gmail.com>
2025-12-10 01:24:10 +0100

    Update auth-client.ts

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,9 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 const { useSession } = createAuthClient();
 
-const authClient = createAuthClient({
-    baseURL: process.env.BETTER_AUTH_URL ?? "http://localhost:3000",
-})
+const authClient = createAuthClient()
 
 
 export { authClient, useSession }
